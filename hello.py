70628a1ed27461be8f98b3c2273fc169ee3a619b
M0ng00se7169 <NickKomarowski@gmail.com>
2020-07-21 12:39:59 +0300

    Launched a flask-bootstrap and added requirements.txt

--- hello.py
+++ hello.py
@@ -1,18 +1,17 @@
-from flask import Flask, request
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<h1>Hello Nekit, your browser is:<br/> {}</h1>'.format(user_agent)
-
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
