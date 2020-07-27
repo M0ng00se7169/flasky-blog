75a15376b6e2766e70e16b4f61f5206b5b5757f8
M0ng00se7169 <NickKomarowski@gmail.com>
2020-07-27 15:55:52 +0300

    Finished authentication process

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -11,6 +13,7 @@ def load_user(user_id):
     
 
 class Role(db.Model):
+    """Model class represents Role model and custom methods"""
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
@@ -21,12 +24,30 @@ class Role(db.Model):
 
 
 class User(UserMixin, db.Model):
+    """Model class represents User model and custom methods"""
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default=False)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({ 'confirm': self.id }).decode('utf-8')
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     @property
     def password(self):
