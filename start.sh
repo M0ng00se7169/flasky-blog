8fc14666a3594118375551f7156615a1a12be718
M0ng00se7169 <NickKomarowski@gmail.com>
2020-07-27 20:21:09 +0300

    Modified start.sh script

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 source flasky/bin/activate
 export FLASK_APP='flasky.py'
 export FLASK_DEBUG=1
+# If you want to export here your Gmail credentials for SMTP messaging,
+# just uncomment next two lines and write your credentials there
+# 
+# export MAIL_USERNAME=<Gmail username>
+# export MAIL_PASSWORD=<bvajqatdmvnodrfg>  Passsword app from Google
 flask run
